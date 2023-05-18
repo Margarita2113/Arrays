0d16f92822cf572e8315d8c0e7ff17270419bf68
Oskina Margarita <rita_2113@mail.ru>
2023-05-18 20:05:19 +0300

    MinMonth

---
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -0,0 +1,13 @@
+package ru.netology.stats;
+
+public class StatsService {
+    public int minSales(int[] sales) {
+        int minMonth = 0;
+        for (int i =0; i < sales.length; i++) {
+            if (sales[i] <= sales[minMonth]) {
+                minMonth = i;
+            }
+        }
+        return minMonth + 1;
+    }
+}
---
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -0,0 +1,19 @@
+package ru.netology.stats;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class StatsServiceTest {
+    @Test
+    public void shouldFindMinMonth() {
+        StatsService service = new StatsService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        int expectedMinMonth = 9;
+        int actualMinMonth = service.minSales(sales);
+
+
+        Assertions.assertEquals(expectedMinMonth, actualMinMonth);
+
+    }
+}
